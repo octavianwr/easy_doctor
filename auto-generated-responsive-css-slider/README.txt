fc64d0ed9e7bfdfd41d4dd564898f61cf58d31ee
octavianwr <octavian_wr@yahoo.com>
2017-05-24 13:14:30 +0700

    UI

---
+++ auto-generated-responsive-css-slider/README.txt
@@ -0,0 +1,4 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/dudleystorey/pen/xDrCw.
+
+ Takes the code of my [responsive CSS slider](http://thenewcode.com/627/Make-A-Responsive-CSS3-Image-Slider) and auto-generates the required styles with progressive JavaScript, based on the number of images and two user-defined timing variables. [More information on my blog](http://thenewcode.com/838/CSSslidy-An-Auto-Generated-Responsive-CSS3-Image-Slider).
+UPDATE: added a [GitHub Page](http://dudleystorey.github.io/CSSslidy/) & [repo](https://github.com/dudleystorey/CSSslidy)
---
+++ form-3/assets/js/scripts.js
@@ -0,0 +1,59 @@
+
+jQuery(document).ready(function() {
+    
+    /*
+        Login form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+	
+	$(document).ready(function () {
+  //called when key is pressed in textbox
+  $("#quantity").keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        //display error message
+        $("#errmsg").html("Hanya Angka").show().fadeOut("slow");
+               return false;
+    }
+   });
+});
+    
+    
+});
